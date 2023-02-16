33e877b7e1e87f885b0b8266452e7dd4f07ebb7a
Nzola Kiampava <nzolakiampava@gmail.com>
2023-02-16 17:54:20 +0100

    Add files via upload

---
+++ database/migrations/2023_02_15_111314_create_sub_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('subcategory_name');
+            $table->string('slug');
+            $table->integer('product_count');
+            $table->string('status')->default('active');
+            $table->integer('category_id');
+            $table->string('category_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
