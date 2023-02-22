4f276a6eb740ae3bab4acc616c729dcde37ce852
Nzola Kiampava <nzolakiampava@gmail.com>
2023-02-22 06:09:29 -1200

    Update createsubcategory.blade.php

--- resources/views/admin/createsubcategory.blade.php
+++ resources/views/admin/createsubcategory.blade.php
@@ -9,27 +9,35 @@ Create SubCategory
             <div class="card-header">
               <h4>Create Sub Category</h4>
             </div>
-            <form action="" method="POST">
+            <form action="{{route('admin.storesubcategory')}}" method="POST">
+                @csrf
                 <div class="card-body">
                     <div class="form-group">
                       <label for="subcategory_name">Enter Sub Category Name</label>
-                      <input type="text" class="form-control" id="subcategory_name" name="category_name" placeholder="Mobile">
+                      <input type="text" class="form-control" id="subcategory_name" name="subcategory_name" placeholder="Mobile">
                     </div>
+
+                    @php
+                    //taking directly from database
+                        $categories = App\Models\Category::where('status','active')->get();
+                    @endphp
 
                     <div class="form-group">
                         <label for="category_name"></label>
                         <select name="category_id" class="form-control form-control-lg">
-                            <option>Option 1</option>
-                            <option>Option 2</option>
-                            <option>Option 3</option>
+                            <option selected>Select one....</option>
+                            @foreach ($categories as $category)
+                                <option value="{{$category->id}}">{{$category->category_name}}</option>
+                            @endforeach
                         </select>
                     </div>
 
                 </div>
+
+                <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Create Sub Category</button>
+                </div>
             </form>
-            <div class="card-footer">
-              <button class="btn btn-primary">Create Sub Category</button>
-            </div>
           </div>
     </div>
 </div>
