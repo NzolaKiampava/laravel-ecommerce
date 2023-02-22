81cd47493e7a233071daba1fbf905a2b098b5354
Nzola Kiampava <nzolakiampava@gmail.com>
2023-02-22 06:10:31 -1200

    Update allcategory.blade.php

--- resources/views/admin/allcategory.blade.php
+++ resources/views/admin/allcategory.blade.php
@@ -51,6 +51,21 @@ All Categories
                                 <td>
                                     <a href="{{ route('admin.editcategory', $category->id) }}" class="btn btn-primary"><i class="fa fa-edit"></i></a>
                                     <a href="{{ route('admin.deletecategory', $category->id) }}" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                                    @if ($category->status == 'active')
+                                        <form action="{{ route('admin.deactivatecategory') }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" value="{{$category->id}}" name="cat_id">
+                                            <input type="submit" value="Deactivate It" class="btn btn-danger mt-2">
+                                        </form>
+                                        <!--<a href="{{ route('admin.deactivatecategory', $category->id) }}" class="btn btn-danger">Deactivate It</a>-->
+                                    @else
+                                        <form action="{{ route('admin.activatecategory') }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" value="{{$category->id}}" name="cat_id">
+                                            <input type="submit" value="Activate It" class="btn btn-success mt-2">
+                                        </form>
+                                        <!--<a href="{{ route('admin.activatecategory', $category->id) }}" class="btn btn-primary">Active It</a>-->
+                                    @endif
                                 </td>
                           </tr>
                         @endforeach
